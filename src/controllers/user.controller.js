45c39e2cac23b980f52b20a33022e8ed42e6a3ac
golu-kumar <golukumar2016006gmail.com>
2025-10-16 02:07:37 +0530

    complete working of register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,8 +16,10 @@ const registerUser = asyncHandler(async (req, res) =>{
   //return res 
 
 
-  const {fullName, email, username, password} = req.body
-  console.log("email : ", email);
+  const {fullName , email, username, password} = req.body 
+  // console.log("email : ", email);
+  
+  
 
   // if (fullName === ""){
   //   throw new ApiError(400, "fullname is required")
@@ -29,23 +31,28 @@ const registerUser = asyncHandler(async (req, res) =>{
     throw new ApiError(400, "All field are required")
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{username}, {email}]
   })
   if (existedUser) {
-    throw new ApiError(409, "User with email or usernaem alread exists")
+    throw new ApiError(409, "User with email or username already exists")
   }
 
   // as req.body given by express as like req.files given by multer
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const covcerImageLocalPath = req.files?.coverImage[0]?.path
+  //  const coverImageLocalPath = req.files?.coverImage[0]?.path   //this line can also write
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
    if (!avatarLocalPath) {
     throw new ApiError(400, 'Avatar file is required')
    }
 
    const avatar = await uploadOnCloudinary(avatarLocalPath)
-   const coverImage = await uploadOnCloudinary(covcerImageLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
    if (!avatar){
     throw new ApiError(400, 'Avatar file is required')
@@ -57,21 +64,21 @@ const registerUser = asyncHandler(async (req, res) =>{
     coverImage: coverImage?.url || "",
     email, 
     password,
-    username: userName.toLowerCase()
+    username: username.toLowerCase()
   
    })
 
    //it store those which we have to not show to any user
-   const createUser = await user.findById(user._id).select( // give all data execpt password and refreshToken
+   const createdUser = await User.findById(user._id).select( // give all data execpt password and refreshToken
     "-password -refreshToken"
    )
 
-   if(!createUser){
+   if(!createdUser){
     throw new ApiError(500, "something went wrong while registering the user")
    }
 
    return res.status(201).json(
-    new ApiResponse(200, createUser, "user registeredn successfully")
+    new ApiResponse(201, createdUser, "User registered successfully")
    )
 })
 
